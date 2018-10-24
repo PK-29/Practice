5749ae854ed470e114caee9d4c529b5984e91a24
Prince Kowser <princekowser@hotmail.com>
2018-10-24 15:57:14 -0400

    add binary search tree

--- codefights.py
+++ codefights.py
@@ -73,3 +73,105 @@ def reverseWords(s):
             return s
 
 print(reverseWords("sky is blue"))
+
+
+#binary tree
+class BinarySTree(object):
+    def __init__(self, root):
+        self.value = root
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+        if value:
+            if value < self.value:
+                if self.left:
+                    self.left.insert(value)
+                
+                else:
+                    self.left = BinarySTree(value)
+                    
+            elif value > self.value:
+                
+                if self.right:
+                    self.right.insert(value)
+                    
+                else:
+                    self.right = BinarySTree(value)
+                    
+            else:
+                self.value  = value
+            
+    #inorder
+    def printTree(self):
+        if self.value:
+            if self.left:
+                self.left.printTree()
+                
+            print(self.value)
+        
+            if self.right:
+                self.right.printTree()
+                
+    def printpreorder(self):
+        if self.value:
+            print (self.value)
+            if self.left:
+                self.left.printpreorder()
+            if self.right:
+                self.right.printpreorder()
+    
+    def printpostorder(self):
+        if self.value:
+            if self.left:
+                self.left.printpostorder()
+            if self.right:
+                self.right.printpostorder()
+            print(self.value)
+    
+    def length(self,size=0):
+        size = size+1
+        if self.value:
+            if self.left:
+                size=self.left.length(size)
+            if self.right:
+                size=self.right.length(size)
+        
+        return size
+    
+    def addtotal(self, value=0):
+        if self.value:
+            value = value + self.value
+            if self.left:
+                value = self.left.addtotal(value)
+            if self.right:
+                value = self.right.addtotal(value)
+        return value
+        
+    def maxvalue(self, maxval=float('inf')):
+        if self.value:
+            maxvalue = self.value
+            if self.value < maxval:
+                maxval = self.value
+            if self.left:
+                maxval = self.left.maxvalue(maxval)
+            if self.right:
+                maxval = self.right.maxvalue(maxval)
+                
+        return maxval
+        
+tree = BinarySTree(-5)
+tree.insert(-2)
+tree.insert(-6)
+tree.insert(-8)
+tree.insert(-3)
+tree.insert(-1)
+tree.insert(-40)
+tree.insert(-9)
+tree.printTree()
+print("break")
+tree.printpreorder()
+print("break")
+tree.printpostorder()
+print("break")
+print(tree.maxvalue())
